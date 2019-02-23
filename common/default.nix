50e88cc8a20f30609e541c88c376fb9a0c4c9e50
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2019-02-22 20:53:15 -0500

    WIP reorganized derivation structure, doesn't work with pounce
    Run manually with:
    
    $(nix-build --fallback --no-out-link common/ -A my-bin)/bin/run

--- common/default.nix
+++ common/default.nix
@@ -5,24 +5,39 @@ with lib.sources;
 let
 
   coursier-jars = import ../3rdparty/coursier;
-
   scalaFiles = sourceFilesBySuffices ./src/main/scala [".scala"];
+  main-class = "sediment.common.WorldLoader";
 
-in stdenv.mkDerivation {
-  name = "sediment-common";
-  srcs = scalaFiles;
-  buildInputs = [ coreutils findutils scala_2_11 ];
-  buildCommand = ''
-    CLASSPATH=$(cat ${coursier-jars}/classpath)
-    
-    echo -n "Coursier classpath: $CLASSPATH"
+in rec {
+  my-lib = stdenv.mkDerivation {
+    name = "sediment-common-lib";
+    srcs = scalaFiles;
+    buildInputs = [ coreutils findutils scala_2_11 ];
+    buildCommand = ''
+      CLASSPATH=$(cat ${coursier-jars}/classpath)
+      
+      echo -n "Coursier classpath: $CLASSPATH"
 
-    mkdir $out
-    files=$(find $srcs -type f)
-    echo "Using files: $files"
-    
-    set -x
-    scalac -classpath $CLASSPATH -d $out $files
-    set +x
-  '';
+      mkdir $out
+      files=$(find $srcs -type f)
+      echo "Using files: $files"
+      
+      set -x
+      scalac -classpath $CLASSPATH -d $out $files
+      set +x
+    '';
+  };
+
+  my-bin = pkgs.writeTextFile { 
+    name = "sediment-common-bin";
+    text = ''
+      DEPS_CLASSPATH=$(cat ${coursier-jars}/classpath)
+      LIB_CLASSPATH=${my-lib}
+      FULL_CLASSPATH="$DEPS_CLASSPATH:$LIB_CLASSPATH"
+      echo "Running with $FULL_CLASSPATH"
+      scala -cp $FULL_CLASSPATH ${main-class}
+    '';
+    executable = true;
+    destination = "/bin/run";
+  };
 }
